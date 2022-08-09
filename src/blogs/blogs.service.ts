cbe97b485ffd4e6ec3a69e231fa31d3c89eda1fa
Dimple Jha <dimple.jha@olacabs.com>
2022-08-09 12:02:07 +0530

    services

--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+
+
+
+// interface Blog{
+//     name:String,
+//     id:number
+
+// }
 @Injectable()
-export class BlogsService {}
+export class BlogsService {
+
+    private readonly blogs:any[]=[];
+    create(data:any){
+        this.blogs.push(data)
+    }
+
+    findData():any[]{
+        return this.blogs;
+    }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,9 @@
-import { Controller , Get} from '@nestjs/common';
-
+import { Controller , Get,Body,Post} from '@nestjs/common';
+import {BlogsService} from '../blogs/blogs.service'
 @Controller('users')
 export class UsersController {
+
+    constructor(private blogService :BlogsService){}
 
     @Get()
     dimple():string{
@@ -11,5 +13,20 @@ export class UsersController {
     @Get("history")
     a():object{
         return {id:1,Name:"sdfghjk"};
+    }
+
+    @Post('userFiles')
+    blogAdd(@Body() record:any){
+        //console.log(record)
+        this.blogService.create(record);
+        //return "user passesd gthe follwing data i.es "
+    }
+
+
+    @Get('blog-list')
+    async blogList():Promise<any[]>{
+        console.log("aaabbb")
+        return this.blogService.findData();
+
     }
 }
