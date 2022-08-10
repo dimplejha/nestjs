7a00c39af03bc9a20bbdb002f06a6420c0c03108
Dimple Jha <dimple.jha@olacabs.com>
2022-08-10 11:35:51 +0530

    gpg added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule ,MiddlewareConsumer,RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { EmployeeModule } from './employee/employee.module';
 import { BlogsService } from './blogs/blogs.service';
 
+import {AuthMiddleware} from './middleware/auth';
 @Module({
   imports: [EmployeeModule],
   controllers: [AppController, UsersController],
   providers: [AppService, BlogsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware)
+    .forRoutes('posts')
+  }
+
+}
